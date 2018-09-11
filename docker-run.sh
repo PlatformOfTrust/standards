bd2406473729774e3486018dcbb348e08ffdf273
Kenneth Söderlund <kenneth.soderlund@gmail.com>
2018-09-11 12:56:53 +0300

    Fix to use /bin/sh instead of bash

--- docker-run.sh
+++ docker-run.sh
@@ -3,4 +3,4 @@ readonly DOCKER=$(which docker)
 readonly BUILD_NAME="doc-build"
 readonly BUILD_SCRIPT="/src/build.sh"
 
-"${DOCKER}" run -v "${PWD}/artifacts:/artifacts" "${BUILD_NAME}" "${BUILD_SCRIPT}"
+"${DOCKER}" run -v "${PWD}/artifacts:/artifacts" "${BUILD_NAME}" /bin/sh "${BUILD_SCRIPT}"
