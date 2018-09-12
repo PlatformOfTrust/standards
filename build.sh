f14af442b8c570702fb967af4245d7c595b176e1
Kenneth Söderlund <kenneth.soderlund@gmail.com>
2018-09-12 15:15:29 +0300

    Fix issue with resource busy

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 readonly WORKDIR="/src"
 readonly ONT_FILE="/src/ontologies/pot.jsonld"
-readonly OUT_FOLDER="/artifacts/"
+readonly OUT_FOLDER="/tmp/html"
+readonly ARTIFACTS="/artifacts"
 readonly THEME="darkly"
 
 cd "${WORKDIR}"
 
+mkdir "${OUT_FOLDER}"
+
 # Need to choose type of export, 2 = Multi-site HTML.
 echo 2 | ontodocs "${ONT_FILE}" -o "${OUT_FOLDER}" -t "Platform Of Trust" --theme="${THEME}"
+
+cp -R "${OUT_FOLDER}"/* "${ARTIFACTS}"/
