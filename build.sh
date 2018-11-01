24625baa759b65701f7132d00cedd9cae7e4fccf
Kenneth Söderlund <kenneth.soderlund@gmail.com>
2018-11-01 09:29:04 +0200

    Use ontology URL instead of file
    Fixes bug where using the ontology file crashes ontospy

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 readonly WORKDIR="/src"
 readonly ONT_FILE="/src/ontologies/pot.jsonld"
+readonly ONT_URL="https://raw.githubusercontent.com/PlatformOfTrust/standards/master/ontologies/pot.jsonld"
 readonly OUT_FOLDER="/tmp/html"
 readonly ARTIFACTS="/artifacts"
 readonly THEME="darkly"
@@ -17,6 +18,6 @@ cp -R "${WORKDIR}/ontologies" "${ARTIFACTS}"/
 cp -R "${WORKDIR}/contexts" "${ARTIFACTS}"/
 
 # Need to choose type of export, 2 = Multi-site HTML.
-echo 2 | ontodocs "${ONT_FILE}" -o "${OUT_FOLDER}" -t "Platform Of Trust" --theme="${THEME}"
+echo 2 | ontodocs "${ONT_URL}" -o "${OUT_FOLDER}" -t "Platform Of Trust" --theme="${THEME}"
 
 cp -R "${OUT_FOLDER}"/* "${ARTIFACTS}"/
