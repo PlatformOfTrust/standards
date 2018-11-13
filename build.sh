886f7ab41a0f2a80695d5523ede6eeb3aac7153c
Kenneth Söderlund <kenneth.soderlund@gmail.com>
2018-11-13 17:41:05 +0200

    Remove ontology URL

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 
 readonly WORKDIR="/src"
 readonly ONT_FILE="/src/ontologies/pot.jsonld"
-readonly ONT_URL="https://raw.githubusercontent.com/PlatformOfTrust/standards/master/ontologies/pot.jsonld"
 readonly OUT_FOLDER="/tmp/html"
 readonly ARTIFACTS="/artifacts"
 readonly THEME="darkly"
@@ -13,11 +12,12 @@ cd "${WORKDIR}"
 
 mkdir "${OUT_FOLDER}"
 
-# Copy over the ontologies and contexts to GH pages
-cp -R "${WORKDIR}/ontologies" "${ARTIFACTS}"/
-cp -R "${WORKDIR}/contexts" "${ARTIFACTS}"/
-
 # Need to choose type of export, 2 = Multi-site HTML.
 echo 2 | ontodocs "${ONT_FILE}" -o "${OUT_FOLDER}" -t "Platform Of Trust" --theme="${THEME}"
 
+# Copy the HTML to the artifacts folder.
 cp -R "${OUT_FOLDER}"/* "${ARTIFACTS}"/
+
+# Copy over the ontologies and contexts to GH pages
+cp -R "${WORKDIR}/ontologies" "${ARTIFACTS}"/
+cp -R "${WORKDIR}/contexts" "${ARTIFACTS}"/
