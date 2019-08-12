b5970614ac52b837f7eacd03e077d439c3268f60
Roman Savran <r.savran@uds.systems>
2019-08-12 12:30:32 +0300

    Redirect
    Adding redirect from root / to /v1/index.html

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ mkdir "${OUT_FOLDER}"
 mkdir "${ARTIFACTS}"
 
 ontospy gendocs "${ONT_FILE}" -o "${OUT_FOLDER}" --title "Platform Of Trust" --theme="${THEME}" --type 2
+sed -i '1iredirect_from: "/"\n' todo.txt
 
 # Copy the HTML to the artifacts folder.
 cp -R "${OUT_FOLDER}"/* "${ARTIFACTS}"/
