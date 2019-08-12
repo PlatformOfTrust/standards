b512d5514be32243cdd499f279b33fe348948f5b
Roman Savran <r.savran@uds.systems>
2019-08-12 12:33:18 +0300

    Update build.sh
    Fix of wrong copy-past example from internet

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ mkdir "${OUT_FOLDER}"
 mkdir "${ARTIFACTS}"
 
 ontospy gendocs "${ONT_FILE}" -o "${OUT_FOLDER}" --title "Platform Of Trust" --theme="${THEME}" --type 2
-sed -i '1iredirect_from: "/"\n' todo.txt
+sed -i '1iredirect_from: "/"\n' "${OUT_FOLDER}/idex.html"
 
 # Copy the HTML to the artifacts folder.
 cp -R "${OUT_FOLDER}"/* "${ARTIFACTS}"/
