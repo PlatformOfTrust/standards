447ca1ce36908f9eaefd094b487b75aaae6d82a2
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-05-05 18:12:16 +0300

    Minor change

--- tools/generators/data_example_from_schema.py
+++ tools/generators/data_example_from_schema.py
@@ -12,28 +12,27 @@ def create_data_example_from_schema(base_dir, target_dir, export_onto_url):
 
         Returns:
     """
-
     for path, subdirs, files in os.walk(base_dir):
         for name in files:
-            if 'Output' not in name and 'Parameters' not in name:
-                with open(os.path.join(path, name), 'r') as json_file:
-                    data = json.load(json_file)
+            with open(os.path.join(path, name), 'r') as json_file:
+                data = json.load(json_file)
 
-                # Build data example json based on schema
-                data_example = dict()
-                for i in data['properties']:
-                    if i != '@type' and i != '@context':
-                        if data['properties'][i].get('properties'):
-                            data_example[i] = {k: data['properties'][i]['properties'][k]['examples'] if 'examples' in data['properties'][i]['properties'][k] else [
-                            ] for k in data['properties'][i]['properties']}
-                        else:
-                            data_example[i] = ''
-                data_example['@context'] = f'{export_onto_url}{path[2:]}/{name[:-5]}/'
+            # Build data example json based on schema
+            data_example = dict()
+            for i in data['properties']:
+                if i != '@type' and i != '@context':
+                    if data['properties'][i].get('properties'):
+                        data_example[i] = {k: data['properties'][i]['properties'][k]['examples'][0]
+                                           if 'examples' in data['properties'][i]['properties'][k] else '' for k in data['properties'][i]['properties']}
+                    else:
+                        data_example[i] = ''
 
-                # Build DataExample files
-                data_example_path = path.replace(base_dir, target_dir)
-                if not os.path.exists(data_example_path):
-                    os.makedirs(data_example_path)
-                with open(os.path.join(data_example_path, name), 'w') as data_example_file:
-                    json.dump(data_example, data_example_file,
-                              indent=4, ensure_ascii=False)
+            data_example['@context'] = f'{export_onto_url}{path[2:]}/{name[:-5]}/'
+
+            # Build DataExample files
+            data_example_path = path.replace(base_dir, target_dir)
+            if not os.path.exists(data_example_path):
+                os.makedirs(data_example_path)
+            with open(os.path.join(data_example_path, name), 'w') as data_example_file:
+                json.dump(data_example, data_example_file,
+                          indent=4, ensure_ascii=False)
