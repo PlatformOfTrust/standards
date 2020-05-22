354ecd3acb788b7db8267a2ed21b48a06ad1dc43
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-05-22 12:07:21 +0300

    Generate shortcut URLs

--- tools/generate.py
+++ tools/generate.py
@@ -66,7 +66,7 @@ def build_rdf_clasess(onto, export_onto_url: str) -> NoReturn:
     for rdf_class in rdf_classes:
         files = rdf_class.get_files()
         for entity_file in files:
-            if 'DataProduct' in rdf_class.entity.name:
+            if 'DataProduct' in rdf_class.entity.name and not is_link_identity_relations(rdf_class):
                 data_to_dump = create_context_from_data_product(
                     rdf_class, entity_file, onto, export_onto_url)
                 write_dump_to_file(CONTEXT_DIR, entity_file, data_to_dump)
--- tools/generators/move_manual.py
+++ tools/generators/move_manual.py
@@ -10,17 +10,16 @@ def move_manual(base_dir, target_dir):
 
             if isinstance(data, list):
                 data = data[0]
-
             if data.get('$id'):
-                target_path = target_dir + '/' + \
-                    '/'.join([x for x in data['$id'].split('/') if x][4:-1])
+                entity_path = [x for x in data['$id'].split('/') if x]
             elif data.get('@context'):
-                target_path = target_dir + '/' + \
-                    '/'.join([x for x in data['@context'].split('/') if x][4:-1])
+                entity_path = [x for x in data['@context'].split('/') if x]
             else:
                 raise KeyError
-            
-            if not os.path.exists(target_path):
-                os.mkdir(target_path)
 
-            copy(os.path.join(base_dir, f), os.path.join(target_path, f))
+            entity_path.insert(4, target_dir)
+            target_path = '/'.join(entity_path[4:-1])
+            target_file = entity_path[-1]
+            os.makedirs(target_path, exist_ok=True)
+            
+            copy(os.path.join(base_dir, f), os.path.join(target_path, f'{target_file}.json'))
