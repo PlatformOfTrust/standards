0d1d630794958f565b5ce4b1b374f502a10d24fd
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-09-08 12:06:28 +0300

    Fix manualPath

--- tools/generators/context_from_data_product.py
+++ tools/generators/context_from_data_product.py
@@ -4,6 +4,17 @@ from generators.commons.extended_properties import nest
 from generators.commons.builders import prop_get_full_id, class_get_full_id, build_attributes
 
 
+def build_manual_path(entity, arr):
+    parent = entity.is_a
+    if entity.name != 'Thing':
+        if entity.manualPath:
+            arr.append(entity.manualPath[0])
+        else:
+            arr.append(entity.name)
+        build_manual_path(parent[0], arr)
+    return arr
+
+
 def create_context_from_data_product(rdf_class, entity_file: Dict[str, Any], onto, export_onto_url: str, PREFIX='pot', VERSION=1.1) -> Dict[str, Any]:
     """Return dict of generated properties to create Context from rdf classes.
 
@@ -18,53 +29,11 @@ def create_context_from_data_product(rdf_class, entity_file: Dict[str, Any], ont
         Returns:
             context_template (dict of str: Any): Dictionary of required parameters
     """
-    corresponds = {
-        "DataProductContext": "DataProductContext",
-        "DataProductOutput": "DataProductOutput",
-        "DataProductParameters": "DataProductParameters",
-        "DataProductParameters": "DataProductParameters",
-        "WeatherForecastDataProductContext": "Weather",
-        "WeatherForecastDataProductOutput": "Weather",
-        "WeatherForecastDataProductParameters": "Weather",
-        "ForecastDataProductContext": "Forecast",
-        "ForecastDataProductOutput": "Forecast",
-        "ForecastDataProductParameters": "Forecast",
-        "LtifDataProductContext": "Ltif",
-        "LtifDataProductOutput": "Ltif",
-        "LtifDataProductParameters": "Ltif",
-        "SensorDataProductContext": "Sensor",
-        "SensorDataProductOutput": "Sensor",
-        "SensorDataProductParameters": "Sensor",
-        "AccuWeatherForecastDataProductContext": "AccuWeather",
-        "AccuWeatherForecastDataProductOutput": "AccuWeather",
-        "AccuWeatherForecastDataProductParameters": "AccuWeather",
-        "DocumentDataProductContext": "Document",
-        "DocumentDataProductOutput": "Document",
-        "DocumentDataProductParameters": "Document",
-        "DocumentSigningDataProductContext": "Signing",
-        "DocumentSigningDataProductOutput": "Signing",
-        "DocumentSigningDataProductParameters": "Signing",
-        "SignSpaceDataProductContext": "SignSpace",
-        "SignSpaceDataProductOutput": "SignSpace",
-        "SignSpaceDataProductParameters": "SignSpace",
-        "PriceForecastDataProductContext": "Price",
-        "PriceForecastDataProductOutput": "Price",
-        "PriceForecastDataProductParameters": "Price",
-        "ElectricityPriceForecastDataProductContext": "Electricity",
-        "ElectricityPriceForecastDataProductOutput": "Electricity",
-        "ElectricityPriceForecastDataProductParameters": "Electricity",
-        "ProjectDataProductContext": "Project",
-        "ProjectDataProductOutput": "Project",
-        "ProjectDataProductParameters": "Project"
-    }
+    manual_path = build_manual_path(rdf_class.entity, [])
+    entity_file['dir'] = '/'.join(manual_path[:0:-1])
+    entity_file['filename'] = f'{manual_path[0]}.jsonld'
+    entity_file['id'] = '/'.join(manual_path[::-1])
 
-    if entity_file.get('id') not in ('DataProductContext', 'DataProductOutput', 'DataProductParameters'):
-        entity_name = entity_file.get('id').split('/')
-        new_path = []
-        for e in entity_name:
-            new_path.append(corresponds[e])
-        entity_file['dir'], entity_file['filename'], entity_file['id'] = '/'.join(new_path[:-1]), f'{new_path[-1]}.jsonld', '/'.join(new_path) 
-        
     context_template = {
         '@version': VERSION,
         rdf_class.entity.name: {"@id": f'pot:{rdf_class.entity.name}'},
